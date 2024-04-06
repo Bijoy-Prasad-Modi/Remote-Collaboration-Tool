74164cc45f27623e20532f9354d4f07b920133c9
Harsh Kumar <harshkumar92200@gmail.com>
2024-04-06 22:30:48 +0530

    Add Sign Up And Login Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import PageNotFound from './components/PageNotFound';
+import SignUp from './pages/SignUp';
+
 
 function App() {
 
   return (
     <>
-    
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='*' element={<PageNotFound />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "../assets/styles/pages/Login.css";
+
+const Login = () => {
+    const [userData, setUserData] = useState({
+        email: "",
+        password: "",
+    });
+
+    function handleChange(event) {
+        setUserData(
+            (prevUserData) => {
+                return { ...prevUserData, [event.target.name]: event.target.value };
+            }
+        );
+    }
+
+    function handleLoginFormSubmit() {
+        event.preventDefault();
+        console.log(userData);
+
+    }
+
+    return <>
+        <>
+            <div className="background">
+                <div className="shape" />
+                <div className="shape" />
+            </div>
+            <form onSubmit={handleLoginFormSubmit} className="text-white">
+                <h3>Login Here</h3>
+
+
+                <label htmlFor="email">Email</label>
+                <input type="email" placeholder="ramesh@gmail.com" name="email" id="email" value={userData.email} onChange={handleChange} />
+
+                <label htmlFor="password">Password</label>
+                <input type="password" placeholder="Password" name="password" id="password" value={userData.password} onChange={handleChange} />
+
+
+                <button className="my-8 p-2 bg-cyan-400">Log In</button>
+
+
+                {/* <div className="social">
+                    <div className="go">
+                        <i className="fab fa-google" /> Google
+                    </div>
+                    <div className="fb">
+                        <i className="fab fa-facebook" /> Facebook
+                    </div>
+                </div> */}
+            </form>
+        </>
+
+    </>;
+};
+
+export default Login;
